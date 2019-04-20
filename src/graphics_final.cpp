012d4e39e391bfbe39f92cb0d82abebb2d670019
ogallagher <owengall@icloud.com>
2019-04-20 00:35:08 -0400

    Added todo for person-visuals branch

--- src/graphics_final.cpp
+++ src/graphics_final.cpp
@@ -8,29 +8,18 @@ Submission for the 3D OpenGL final project.
 It's a top-down shooter with SuperHot-like mechanics, where time is faster when the player
 moves, and slower when the player stands still.
 
-TODO
-- enemy
-	- point at player method
-- player
-	- move for arrow keys
-	- score variable
-	- variables for shooting (ammo, reload time)
-- bullet class
-	- collision with: person, obstacle
-- obstacle class
-- person
-	- shoot method
-	- die method
-	- walking animation (for later)
-- world/app
-	- modify World::speed based on framerate
-	- idle method: update animations and motions
-	- cursor-world projection: draw a ray representing the current mouse position (for later)
-- research/questions
-	- how to use sound with opengl/glut (so we could have sound effects)
-	- how should we set up the environment?
-	- how should we spawn enemies?
-	- how should the enemies behave (how they follow the player and shoot)?
+TODO <person-visuals>: draw person full body
+- torso
+	- rotates according to heading
+- head
+	- 
+- legs
+	- rotates according to velocity (cardinal directions)
+	- walking animation
+- color
+- material properties
+	- uses color
+	- uses ambient, diffuse, and specular light
 
 */
 
